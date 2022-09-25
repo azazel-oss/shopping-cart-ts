391e29abd49113c734bee2270840c98c83fd1d7b
Asad Mahmood <asadmhmd1234@gmail.com>
2022-09-25 13:01:55 +0530

    Add shopping cart modal

---
+++ src/components/Cart/Cart.tsx
@@ -0,0 +1,24 @@
+import styles from "./Cart.module.css";
+const Cart = () => {
+  const cartItems = (
+    <ul className={styles["cart-items"]}>
+      {[{ id: "c1", name: "Sushi", amount: 2, price: 12.99 }].map((item) => (
+        <li>{item.name}</li>
+      ))}
+    </ul>
+  );
+  return (
+    <div>
+      {cartItems}
+      <div className={styles.total}>
+        <span>Total Amount</span>
+        <span>35.22</span>
+      </div>
+      <div className={styles.actions}>
+        <button className={styles.button}>Close</button>
+      </div>
+    </div>
+  );
+};
+
+export default Cart;
