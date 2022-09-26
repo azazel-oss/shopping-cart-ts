fc1c3d9959c756067e116671881d1c176744c915
Asad Mahmood <asadmhmd1234@gmail.com>
2022-09-26 15:13:28 +0530

    Add animation to cart button in header

--- src/components/Layout/HeaderCartButton.tsx
+++ src/components/Layout/HeaderCartButton.tsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import CartContext, { CartContextType, Item } from "../../contexts/cartContext";
 import CartIcon from "../Cart/CartIcon";
 import styles from "./HeaderCartButton.module.css";
@@ -8,6 +8,7 @@ type Props = {
 };
 
 const HeaderCartButton = ({ onClick }: Props) => {
+  const [isButtonBumped, setIsButtonBumped] = useState(false);
   const cart = useContext(CartContext) as CartContextType;
 
   const uniqueCartItemsNumber = cart.items.reduce(
@@ -15,8 +16,23 @@ const HeaderCartButton = ({ onClick }: Props) => {
       acc.some((ele) => ele.id === curr.id) ? acc : [...acc, curr],
     []
   );
+
+  const buttonClasses = `${styles.button} ${isButtonBumped ? styles.bump : ""}`;
+
+  useEffect(() => {
+    if (cart.items.length === 0) {
+      return;
+    }
+    setIsButtonBumped(true);
+    const timer = setTimeout(() => {
+      setIsButtonBumped(false);
+    }, 300);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [cart.items]);
   return (
-    <button onClick={onClick} className={styles.button}>
+    <button onClick={onClick} className={buttonClasses}>
       <span className={styles.icon}>
         <CartIcon />
       </span>
